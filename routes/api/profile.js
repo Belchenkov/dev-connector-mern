07ebe57a28936c20fc25a877fc907165cc5e0255
Aleksey Belchenkov <u608110@gmail.com>
2019-03-02 11:54:11 +0300

    Profile Field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,9 @@ const Profile = require('../../models/Profile');
 // Load User Profile
 const User = require('../../models/User');
 
+// Load Input Validation
+const validateProfileInput = require('../../validation/profile');
+
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
@@ -25,6 +28,7 @@ router.get(
         const errors = {};
 
         Profile.findOne({ user: req.user.id })
+            .populate('user', ['name', 'avatar'])
             .then(profile => {
                 if (!profile) {
                     errors.noprofile = 'There is no profile for this user';
@@ -42,6 +46,14 @@ router.post(
     '/',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+        // Check Validation
+        if (!isValid) {
+            // Return any errors with 400 status
+            return res.status(400).json(errors);
+        }
+
         // Get fields
         const profileFields = {};
 
