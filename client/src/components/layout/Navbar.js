7539f9335aee62cb77e99a605517b809dc084d6b
Aleksey Belchenkov <u608110@gmail.com>
2019-03-08 10:23:54 +0300

    React Router Setup

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4 p-0">
                 <div className="container">
-                    <a className="navbar-brand" href="landing.html">DevConnector</a>
+                    <Link className="navbar-brand d-flex align-items-center" to="/">
+                        <div className="mr-1">
+                            <img src="https://img.icons8.com/color/30/000000/electrical.png" />
+                        </div>
+                        <div>
+                            DevConnector
+                        </div>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -13,17 +21,37 @@ class Navbar extends Component {
                     <div className="collapse navbar-collapse" id="mobile-nav">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="profiles.html"> Developers
-                                </a>
+                                <Link className="nav-link d-flex align-items-center" to="/profiles">
+                                    <div className="mr-1">
+                                        <img src="https://img.icons8.com/office/15/000000/user-group-man-man.png" />
+                                    </div>
+                                    <div>
+                                        Developers
+                                    </div>
+                                </Link>
                             </li>
                         </ul>
 
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="register.html">Sign Up</a>
+                                <Link className="nav-link d-flex align-items-center" to="/register">
+                                    <div className="mr-1">
+                                        <img src="https://img.icons8.com/office/15/000000/add-key.png" />
+                                    </div>
+                                    <div>
+                                        Sign Up
+                                    </div>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="login.html">Login</a>
+                                <Link className="nav-link d-flex align-items-center" to="/login">
+                                    <div className="mr-1">
+                                        <img src="https://img.icons8.com/dusk/15/000000/password.png" />
+                                    </div>
+                                    <div>
+                                        Login
+                                    </div>
+                                </Link>
                             </li>
                         </ul>
                     </div>
