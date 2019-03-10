c33bcf06a048cbb7d00c183701f114de63fb7b6b
Aleksey Belchenkov <u608110@gmail.com>
2019-03-10 21:07:24 +0300

    Login Form Functionality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
 import classnames from 'classnames';
 
 class Login extends Component {
@@ -17,17 +20,27 @@ class Login extends Component {
         this.setState({ [e.target.name]: e.target.value });
     };
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
 
-        const user = {
+        const userData = {
             email: this.state.email,
             password: this.state.password,
         };
 
-        axios.post('/api/users/login', user)
-            .then(res => console.log(res.data))
-            .catch(err => console.error(err.response.data))
+        this.props.loginUser(userData);
     };
 
     render() {
@@ -45,39 +58,43 @@ class Login extends Component {
                                 </h1>
                                 <p className="lead text-center">Sign in to your DevConnector account</p>
                                 <form  onSubmit={this.onSubmit}>
-                                    <div className="d-flex w-100">
-                                        <div className="mr-2">
+                                    <div className="row">
+                                        <div className="col-md-1">
                                             <img src="https://img.icons8.com/clouds/45/000000/gmail.png" />
                                         </div>
-                                        <div className="form-group w-100">
+                                        <div className="col-md-11 form-group">
                                             <input
                                                 type="email"
                                                 className={classnames('form-control form-control-lg', {
-                                                    'is-invalid': errors.name
+                                                    'is-invalid': errors.email
                                                 })}
                                                 value={this.state.email}
                                                 placeholder="Email Address"
                                                 name="email"
                                                 onChange={this.onChange}
                                             />
+                                            {errors.email && (<div className="d-flex w-100 invalid-feedback">{errors.email}</div>)}
                                             <small>
                                                 This site uses Gravatar so if you want a
                                                 profile image, use a Gravatar email
                                             </small>
                                         </div>
                                     </div>
-                                    <div className="form-group d-flex align-items-center">
-                                        <div className="mr-2">
+                                    <div className="row">
+                                        <div className="col-md-1">
                                             <img src="https://img.icons8.com/clouds/45/000000/password.png" />
                                         </div>
-                                        <input
-                                            type="password"
-                                            className="form-control form-control-lg"
-                                            value={this.state.password}
-                                            placeholder="Password"
-                                            name="password"
-                                            onChange={this.onChange}
-                                        />
+                                        <div className="form-group col-md-11">
+                                            <input
+                                                type="password"
+                                                className="form-control form-control-lg"
+                                                value={this.state.password}
+                                                placeholder="Password"
+                                                name="password"
+                                                onChange={this.onChange}
+                                            />
+                                            {errors.password && (<div className="d-flex w-100 invalid-feedback">{errors.password}</div>)}
+                                        </div>
                                     </div>
                                     <button
                                         type="submit"
@@ -96,4 +113,15 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
