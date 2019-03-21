2cd751812bba33ea7ba7582e337d7bba84b17e3e
Aleksey Belchenkov <u608110@gmail.com>
2019-03-21 06:49:52 +0300

    Experience Functionality

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
     constructor(props) {
@@ -22,10 +23,26 @@ class AddExperience extends Component {
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
 
-        console.log('submit');
+        const expData = {
+            company: this.state.company,
+            title: this.state.title,
+            location: this.state.location,
+            from: this.state.from,
+            to: this.state.to,
+            current: this.state.current,
+            description: this.state.description
+        };
+
+        this.props.addExperience(expData, this.props.history);
     };
 
     onChange = (e) => {
@@ -67,6 +84,13 @@ class AddExperience extends Component {
                                     value={this.state.title}
                                     onChange={this.onChange}
                                     error={errors.title}
+                                />
+                                <TextFieldGroup
+                                    placeholder="Location"
+                                    name="location"
+                                    value={this.state.location}
+                                    onChange={this.onChange}
+                                    error={errors.location}
                                 />
                                 <h6>From Date</h6>
                                 <TextFieldGroup
@@ -122,6 +146,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+    addExperience: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 };
@@ -131,4 +156,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(withRouter(AddExperience));
