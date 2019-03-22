13523bcc19359567db5f5719efddd26c4010fc78
Aleksey Belchenkov <u608110@gmail.com>
2019-03-22 06:52:46 +0300

    Dashboard Experience Display

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
+import isEmpty from '../../validation/is-empty';
+
+class Experience extends Component {
+    render() {
+        const experience = this.props.experience.map(exp => (
+            <tr key={exp._id}>
+                <td>{exp.company}</td>
+                <td>{exp.title}</td>
+                <td>
+                    <Moment format="YYYY/MM/DD">{exp.from}</Moment> - {' '}
+                    { isEmpty(exp.to) ? 'Now' :  <Moment format="YYYY/MM/DD">{exp.to}</Moment> }
+                </td>
+                <td>
+                    <button className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        ));
+
+        return (
+            <div>
+                <h4 className="mb-4">Experience Credentials</h4>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Title</th>
+                            <th>Years</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {experience}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+Experience.propTypes = {
+    profile: PropTypes.object.isRequired
+};
+
+export default connect(null)(withRouter(Experience));
