d76261e8ac56848d08fc746968c1bcf355cf562f
Aleksey Belchenkov <u608110@gmail.com>
2019-03-28 06:38:32 +0300

    Like & Unlike Posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -20,6 +20,26 @@ export const addPost = postData => dispatch => {
         }))
 };
 
+// Add Like
+export const addLike = id => dispatch => {
+    axios.post(`/api/posts/like/${id}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+    axios.post(`/api/posts/unlike/${id}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+};
+
 // Get Posts
 export const getPosts = () => dispatch => {
     dispatch(setPostLoading());
