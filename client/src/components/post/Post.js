a63227ae99619015e0b572eed57325271d41d93d
Aleksey Belchenkov <u608110@gmail.com>
2019-03-30 09:44:07 +0300

    Comment Display & Deletet

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentFeed from '../post/CommentFeed';
 import Spinner from "../common/Spinner";
 import { Link } from 'react-router-dom';
 import { getPost } from "../../actions/postActions";
@@ -26,6 +27,7 @@ class Post extends Component {
                         showActions={false}
                     />
                     <CommentForm postId={post._id} />
+                    <CommentFeed postId={post._id} comments={post.comments} />
                 </div>
             );
         }
